e79179aab8b47560d26ee630fa724f2c580a7aed
Olivier <olivier.bondu@gmail.com>
2013-11-05 16:26:49 +0100

    Adding plotting of several vars

--- analysis.py
+++ analysis.py
@@ -74,6 +74,10 @@ NElectrons = []
 NPhotons = []
 NJets = []
 NBJets = []
+MET = []
+## defining the plots to be drawn
+allPlots = [invMassSel, NMuons, NElectrons, NPhotons, NJets, NBJets, MET]
+
 hadB = TLorentzVector()
 lepB = TLorentzVector()
 hadWq = TLorentzVector()
@@ -84,6 +88,14 @@ met = TLorentzVector()
 
 outFile = TFile("histos_all.root","RECREATE")
 hs = THStack('hs','invariant Mass')
+hs_invMassSel = THStack('hs_invMassSel','hs_invMassSel')
+hs_NMuons = THStack('hs_NMuons','hs_NMuons')
+hs_NElectrons = THStack('hs_NElectrons','hs_NElectrons')
+hs_NPhotons = THStack('hs_NPhotons','hs_NPhotons')
+hs_NJets = THStack('hs_NJets','hs_NJets')
+hs_NBJets = THStack('hs_NBJets','hs_NBJets')
+hs_MET = THStack('hs_MET','hs_MET')
+allStacks = [hs_invMassSel, hs_NMuons, hs_NElectrons, hs_NPhotons, hs_NJets, hs_NBJets, hs_MET]
 
 for file in AllSample:
     nameSample = AllSample[countFile]
@@ -98,61 +110,71 @@ for file in AllSample:
     NPhotons.append(TH1F('NPhotons_'+ nameSample,'Photons nb',10,0,10))
     NJets.append(TH1F('NJets_'+ nameSample,'Jets nb',10,0,10))
     NBJets.append(TH1F('NBJets_'+ nameSample,'BJets nb',10,0,10))
+    MET.append(TH1F('MET_'+ nameSample,'MET',50,0,200))
 
     chain.Add("files/"+nameSample+".root")
     nbEntries = chain.GetEntries()
-#    nbEntries = 10
+#    nbEntries = 5000
     for i in xrange(nbEntries):
         if(nbEntries > 10000 and i % (nbEntries/10) == 0): print "\tProcessing entry {0} / {1} ({2}% done)".format(i, nbEntries, ceil(float(i)/float(nbEntries)*100.))
         chain.GetEntry(i)
         TriggerIsoMu24 = chain.triggerIsoMu24
-        AllMuons = []; nMuon = chain.NMuon
-        AllElectrons = []; nElectron = chain.NElectron
-        AllPhotons = []; nPhoton = chain.NPhoton
-        AllJets = []; nJet = chain.NJet
-        AllBJets = []; nBJet = 0
-
 #######################################
 ## TTBar-like selection
 # Trigger
         if(not TriggerIsoMu24): continue
 # Muon selection
+        nMuon = chain.NMuon
         if(nMuon < 1): continue
+        AllMuons = []
         FillTheMuonCollection()
         if(AllMuons[0].Pt() < 24.): continue
         if(not (AllMuons[0].IsIsolated())): continue
 # Jet selection
+        nJet = chain.NJet
+
         if(nJet < 4): continue
+        AllJets = []
         FillTheJetCollection()
         if(AllJets[3].Pt() < 30.): continue
 # B-tag selection
+        nBJet = 0
         for k in xrange(3):
             if(AllJets[k].GetBTagDiscriminator() > 1.): nBJet += 1
 # MET selection
         FillTheOtherCollections()
-        if(met.Pt() < 10.): continue
+#        if(met.Pt() < 10.): continue
 # Filling other event info
+        nElectron = chain.NElectron
+        nPhoton = chain.NPhoton
         eventWeight = chain.EventWeight
         weight = eventWeight * TriggerIsoMu24 * IntLumiWeight[countFile]
 # construction of TTBar system
         TTBar = AllMuons[0] + AllJets[0] + AllJets[1] + AllJets[2] + AllJets[3]
+# fill plots
         invMassSel[countFile].Fill(TTBar.M(), weight)        
         NMuons[countFile].Fill(nMuon, weight)
         NElectrons[countFile].Fill(nElectron, weight)
+        NPhotons[countFile].Fill(nPhoton, weight)
         NJets[countFile].Fill(nJet, weight)
         NBJets[countFile].Fill(nBJet, weight)
+        MET[countFile].Fill(met.Pt(), weight)
 #        [countFile].Fill(, weight)
 
 #######################################
+
+
     if(countFile > 0):
         invMassSel[countFile].SetFillColor(SampleColor[countFile])
-        invMassSel[countFile].SetFillStyle(3002)
         hs.Add(invMassSel[countFile])
+        for istack in xrange(len(allStacks)):
+            allPlots[istack][countFile].SetFillColor(SampleColor[countFile])
+            allStacks[istack].Add(allPlots[istack][countFile])
     countFile = countFile + 1
 
 print ' creating canvas'
 c = TCanvas("c", "c", 800, 1000)
-c.clear()
+c.Clear()
 c.Divide(1,2)
 c.cd(1)
 hs.Draw('hist')
@@ -164,6 +186,20 @@ gPad.SetLogy(1)
 hs.Draw('hist')
 invMassSel[0].Draw('pesame')
 c.Print("InvMass.gif")
+
+for istack in xrange(len(allStacks)):
+    c.Clear()
+    c.Divide(1,2)
+    c.cd(1)
+    allStacks[istack].Draw('hist')
+    allPlots[istack][0].Draw('pesame')
+    allPlots[istack][0].SetMarkerStyle(20)
+    allPlots[istack][0].SetMarkerSize(.5)
+    c.cd(2)
+    gPad.SetLogy(1)
+    allStacks[istack].Draw('hist')
+    allPlots[istack][0].Draw('pesame')
+    c.Print(allStacks[istack].GetName() + ".gif")
 
 outFile.Write()
 outFile.Close()
