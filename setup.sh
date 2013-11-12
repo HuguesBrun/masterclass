0e404f31993e9c125a253474e2bfad8e9337bcd9
Olivier <olivier.bondu@gmail.com>
2013-11-12 10:58:30 +0100

    Adding verbosity while setting up the environment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 # SETUP ROOT
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.3/x86_64-slc5/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.32.00/x86_64-slc5-gcc43-opt/root/bin/thisroot.sh
